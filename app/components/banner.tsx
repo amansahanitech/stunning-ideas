37ac00668a52a458e8cee77de8e5f5b83d680d7d
amansahanitech <137435651+amansahanitech@users.noreply.github.com>
2023-06-23 22:43:32 +0530

    Update banner.tsx

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -5,13 +5,14 @@ const Banner = () => {
     return (
       <div className="relative">
         <Image
-          className="w-full h-auto"
+          className="w-full h-fit z-0"
           src={bgImage}
           priority={true}
           alt="Almond Blossoms Painting"
         />
        {/**
-        *  <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-black bg-opacity-10">
+        *  
+        * <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-black bg-opacity-10">
           <h1 className="text-white text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-extralight text-center">
             Welcome to Our Shop
           </h1>
