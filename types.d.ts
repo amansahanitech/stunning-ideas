feff93d1feaa349cc5fb9595cf075e4cfd9e2414
amansahanitech <137435651+amansahanitech@users.noreply.github.com>
2023-06-24 00:19:59 +0530

    Add files via upload

---
+++ types.d.ts
@@ -0,0 +1,15 @@
+type description = {
+    About : string,
+    customerRequirements : string[]
+}
+
+type productType = {
+    name: string,
+    price: number,
+    image: string[],
+    meta: string,
+    light:string[],
+    Size: string[],
+    description: description,
+  }
+
